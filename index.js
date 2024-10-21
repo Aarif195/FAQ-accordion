57a8bfccfc6df747dc4fc5565b95fd9ad160f2e4
Aarif <adebayoabdulazeez195@gmail.com>
2024-10-21 04:12:00 -0700

    correction of codes

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 // Toggle between adding and removing the "active" class.
 // show "- image" after clicking the content-container
 
-const accordion = document.getElementsByClassName('content-container');
+// const accordion = document.getElementsByClassName('content-container');
 
-console.log(accordion);
+// console.log(accordion);
 
-for (let i = 0; i < accordion.length; i++) {
+// for (let i = 0; i < accordion.length; i++) {
 
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-  })
+//   accordion[i].addEventListener('click', function () {
+//     this.classList.toggle('active');
+//   })
 
   
-};
+// };
